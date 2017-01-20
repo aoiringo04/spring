867b30d94efb86e1b6ea709a33ea0f208e471b01
~obomi~ <~aoiringo04@naver.com~>
2017-01-20 11:26:13 +0900

    아이디 중복확인 기능 구현

--- Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
+++ Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
@@ -2,13 +2,17 @@ package com.gura.spring.users.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.gura.spring.users.dto.UsersDto;
 import com.gura.spring.users.service.UsersService;
 
 // Component 스캔 시 bean 이 되고 또한 컨트롤러 역할을 할 수 있도록
@@ -17,6 +21,18 @@ public class UsersController{
 	// 의존객체 주입 되도록
 	@Autowired
 	private UsersService usersService;
+	
+	// "/users/signup.do" 요청 처리
+	@RequestMapping("/users/signup")
+	public ModelAndView signup(HttpServletRequest request, @ModelAttribute UsersDto dto){
+		// 서비스를 이용해서 회원정보를 저장한다.
+		usersService.insert(dto);
+		ModelAndView mView = new ModelAndView();
+		mView.addObject("msg", dto.getId() + "회원님 가입되었습니다.");
+		mView.addObject("redirectUri", request.getContextPath());
+		mView.setViewName("users/alert");
+		return mView;
+	}
 	
 	// ajax "/users/checkid.do" 요청 처리
 	@RequestMapping("/users/checkid")
--- Spring09_Git/src/main/java/com/gura/spring/users/service/UsersServiceImpl.java
+++ Spring09_Git/src/main/java/com/gura/spring/users/service/UsersServiceImpl.java
@@ -19,8 +19,7 @@ public class UsersServiceImpl implements UsersService{
 
 	@Override
 	public void insert(UsersDto dto){
-		// TODO Auto-generated method stub
-
+		usersDao.insert(dto);
 	}
 
 	@Override
